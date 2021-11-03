ea061e6c3322e0e6c3380c5a73b34235adb81a69
Martin Latta <lattam@tentwentyfour.lu>
2021-11-03 16:02:28 +0100

    chore: update jest packages

--- jest.conf.js
+++ jest.conf.js
@@ -2,4 +2,5 @@ module.exports = {
   moduleFileExtensions: ["ts", "js"],
   testMatch:            ["**/*-jest\.ts"],
   transform:            { "\\.ts$": "ts-jest" },
+  testRunner:           "jest-jasmine2",
 };
